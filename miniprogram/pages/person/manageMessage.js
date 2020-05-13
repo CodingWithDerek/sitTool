bbbb65d2e5f267dc7db117758bc3c9c6630778ef
chilo-hzl <852772745@qq.com>
2020-05-13 14:23:53 +0800

    优雅的处理用户订阅消息逻辑

--- miniprogram/pages/person/manageMessage.js
+++ miniprogram/pages/person/manageMessage.js
@@ -5,7 +5,13 @@ Page({
    * 页面的初始数据
    */
   data: {
-    subscribeFlag: false
+    flag1:false,
+    flag2:false,
+    flag3:false,
+    flag4:false,
+    flag5:false,
+    flag6:false,
+    flag7:false
   },
 
   /**
@@ -19,25 +25,65 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function() {
-    var that = this
-    wx.getStorage({
-      key: 'subscribeFlag',
-      success: function(res) {
-        that.setData({
-          subscribeFlag: res.data
-        })
-      },
-      fail(err) {
-        console.log(err)
-      }
-    })
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    var that = this
+    this.setData({
+      flag1:false,
+      flag2:false,
+      flag3:false,
+      flag4:false,
+      flag5:false,
+      flag6:false,
+      flag7:false
+    })
+    wx.getSetting({
+      withSubscriptions: true,
+      success(res) {
+        //console.log(res)
+        if (res.subscriptionsSetting.mainSwitch == true && res.subscriptionsSetting.NqBtoPiR4u1v0pcuW_7Ygtb0O5o9VJN6JnsJHMgwX6g == undefined) {
+          that.setData({
+            flag1: true
+          })
+        }
+        if (res.subscriptionsSetting.mainSwitch == true && res.subscriptionsSetting["IWuRiPxhYaRciLvy3S1h6WX50PZLZ3hl4-VHjHAKsdw"] == undefined) {
+          that.setData({
+            flag2: true
+          })
+        }
+        if (res.subscriptionsSetting.mainSwitch == true && res.subscriptionsSetting.NqBtoPiR4u1v0pcuW_7Ygtb0O5o9VJN6JnsJHMgwX6g == "accept") {
+          that.setData({
+            flag3: true
+          })
+        }
+        if (res.subscriptionsSetting.mainSwitch == true && res.subscriptionsSetting["IWuRiPxhYaRciLvy3S1h6WX50PZLZ3hl4-VHjHAKsdw"] == "accept") {
+          that.setData({
+            flag4: true
+          })
+        }
+        if (res.subscriptionsSetting.mainSwitch == true && res.subscriptionsSetting.NqBtoPiR4u1v0pcuW_7Ygtb0O5o9VJN6JnsJHMgwX6g == "reject") {
+          that.setData({
+            flag5: true
+          })
+        }
+        if (res.subscriptionsSetting.mainSwitch == true && res.subscriptionsSetting["IWuRiPxhYaRciLvy3S1h6WX50PZLZ3hl4-VHjHAKsdw"] == "reject") {
+          that.setData({
+            flag6: true
+          })
+        }
+        if (res.subscriptionsSetting.mainSwitch == false) {
+          that.setData({
+            flag7: true
+          })
+        }
+        //console.log(that.data)
+      }
+    })
   },
 
   /**
@@ -58,7 +104,6 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function() {
-
   },
 
   /**
@@ -74,24 +119,35 @@ Page({
   onShareAppMessage: function() {
 
   },
-  subscribeInfo: function() {
+  subscribeInfo_sentJob:function(){
     var that = this
     wx.requestSubscribeMessage({
-      tmplIds: ['H9oeXwgzNaiub038QJ1rSb1L7V_I6H5HOhVU6VMxaRc',
-        'NqBtoPiR4u1v0pcuW_7Ygtb0O5o9VJN6JnsJHMgwX6g'
-      ],
-      success(res) {
-        if (res.H9oeXwgzNaiub038QJ1rSb1L7V_I6H5HOhVU6VMxaRc=="accept"){
-          wx.setStorage({
-            key: 'subscribeFlag',
-            data: true,
-          })
-          that.setData({
-            subscribeFlag: true
-          })
-        }
+      tmplIds: ["NqBtoPiR4u1v0pcuW_7Ygtb0O5o9VJN6JnsJHMgwX6g"],
+      success:function(res){
+        that.onShow()
       },
-      fail(err) {
+      fail:function(err){
+        console.log(err)
+      }
+    })
+  },
+  subscribeInfo_student:function(){
+    var that =this
+    wx.requestSubscribeMessage({
+      tmplIds: ["IWuRiPxhYaRciLvy3S1h6WX50PZLZ3hl4-VHjHAKsdw"],
+      success: function (res) {
+        that.onShow()
+      },
+      fail: function (err) {
+        console.log(err)
+      }
+    })
+  },
+  goSetting:function(){
+    wx.openSetting({
+      success:function(res){
+      },
+      fail:function(err){
         console.log(err)
       }
     })
